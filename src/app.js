a04fab3acc63a233284e696e45b8d0a1f4bc7321
cnvorous <cnvorous@gmail.com>
2022-01-12 22:33:32 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com"];
+  let newDomain = [];
+
+  var htmlInsert = document.getElementById("domain-name");
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        newDomain.push(pronoun[i] + adj[j] + noun[k] + extension[0]);
+      }
+    }
+  }
+
+  console.log(newDomain);
+  for (let i = 0; i < newDomain.length; i++) {
+    htmlInsert.innerHTML += "<li>" + newDomain[i] + "</li>";
+  }
 };
